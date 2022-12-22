1d767cf90a328bb906108f04310297c4c9912cbf
Yi Han <chris.yi.han@outlook.com>
2022-12-22 09:59:52 +0800

    Add function

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from transaction import Transaction
 from price import Price
 
 if __name__ == "__main__":
-    # t = Transaction(688017, "/Users/y1han/databases/STAR Board/")
+    t = Transaction(688363, "/Users/y1han/databases/STAR Board/")
     # t.avg_cum_trade_quantity.plot()
+    t.intraday_price_series().plot()
 
-    p = Price(688017, "/Users/y1han/databases/STAR Board/")
+    # p = Price(688017, "/Users/y1han/databases/STAR Board/")
     # p.order_quantity("2022-07-04").plot()
     # p.intraday_price_series("2022-07-04").plot(secondary_y=True, alpha=0.5, color="grey")
     # p.bid_ask_spread("2022-07-04").plot(secondary_y=True, alpha=0.5, color="grey", marker='.', linestyle='none')
     # p.avg_order_quantity.plot()
     # p.avg_bid_ask_spread.plot.line(secondary_y=True, alpha=0.5, color="grey", marker='.', linestyle='none')
-    p.signature_plot_series(resample_period="1H").plot.scatter(x='lag', y='var')
+    # p.signature_plot_series(resample_period="1H").plot.scatter(x='lag', y='var')
     # p.price_series(resample_period="1H").plot()
     plt.show()
--- transaction.py
+++ transaction.py
@@ -4,11 +4,12 @@ from utils import read_data_flow
 
 OPEN_TIME = time(9, 30, 0)
 CLOSE_TIME = time(15, 0, 0)
+BREAK_TIME = [time(11, 30, 0), time(13, 0, 0)]
 
 
 class Transaction:
     def __init__(self, code, file_path):
-        self.transaction_flow = read_data_flow(code=code, file_path=file_path, data_type="Transaction")
+        self.transaction_flow = read_data_flow(code=code, file_path=file_path, data_type="T")
 
     def day_cum_trade_quantity(self, date: str = "2022-07-04"):
         return self.transaction_flow["TradeQty"][self.transaction_flow.index.date.astype(str) == date].cumsum()
@@ -17,3 +18,14 @@ class Transaction:
     def avg_cum_trade_quantity(self):
         resample = self.transaction_flow["TradeQty"].resample("3s").sum().between_time(OPEN_TIME, CLOSE_TIME)
         return resample.groupby([resample.index.time]).mean().cumsum()
+
+    def intraday_price_series(self, date: str = "2022-07-04"):
+        return self.transaction_flow["TradePrice"][self.transaction_flow.index.date.astype(str) == date].resample(
+            "3s").last().ffill().between_time(OPEN_TIME, CLOSE_TIME).between_time(BREAK_TIME[1],
+                                                                                  BREAK_TIME[0],
+                                                                                  include_start=False,
+                                                                                  include_end=False)
+
+    def price_series(self, resample_period="1D"):
+        return self.transaction_flow["TradePrice"].resample(resample_period).last().dropna().between_time(
+            OPEN_TIME, CLOSE_TIME)
